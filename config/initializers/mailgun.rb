5bd5e98c24832ee1defe2e0709bbff91521d5e2e
Avinash <justavi217@gmail.com>
2017-10-24 18:10:10 -0400

    removed irrelavent file

--- config/initializers/mailgun.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-config_file = "#{Rails.root}/config/mailgun.yml"
-file_contents = ERB.new(File.read(config_file)).result
-MAILGUN_CONFIG = YAML.safe_load(file_contents)[Rails.env]
-
-Mailgun.configure do |config|
-  config.api_key = MAILGUN_CONFIG[:mailgun_api_key]
-end
